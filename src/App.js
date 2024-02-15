ee319e82d8adef3b5cf837d4b81c098dc63b5601
carterwilson12 <wilsoncr2@g.cofc.edu>
2024-02-15 13:40:07 -0500

    WIP selector work

--- src/App.js
+++ src/App.js
@@ -1,27 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
-import ListGroup from 'react-bootstrap/ListGroup';
+import AutocompleteHint from './components/autocomplete';
+import { Button, ButtonGroup, ToggleButtonGroup, ToggleButton} from '@mui/material';
+import React from 'react';
+const mystyle = {
+  color: "white",
+  backgroundColor: "DodgerBlue",
+  
+};
+const wip_ids = ["LS12231232",
+  "MS1732231362",
+  "MS1732213213",
+  "MS1732132123",
+  "MS1732332323",]
 
+  
 function App() {
-  const alertClicked = () => {
-    alert('You clicked the third ListGroupItem');
+  const [wip_id, setWIP] = React.useState<String | null>('');
+  const handleWIPselect = (event, newWIP) => {
+    setWIP(newWIP);
   };
   return (
+    
     <div className="App">
-        <div className="WIP-selector">  
-          <ListGroup defaultActiveKey="#link1">
-            <ListGroup.Item action href="#link1">
-              Link 1
-            </ListGroup.Item>
-            <ListGroup.Item action href="#link2" disabled>
-              Link 2
-            </ListGroup.Item>
-            <ListGroup.Item action onClick={alertClicked}>
-              This one is a button
-            </ListGroup.Item>
-          </ListGroup>
+        <div className="SearchBar">  
+          <AutocompleteHint></AutocompleteHint>
         </div>
+        
+      <ToggleButtonGroup value={wip_id}
+      exclusive
+      onChange={handleWIPselect}
+      className="WIP-list" orientation="vertical" aria-label="Vertical button group" variant="contained">
+        {Object.entries(wip_ids).map(wip_id=>{
+          console.log(wip_id)
+          return(
+            <ToggleButton value={wip_id}className="WIP-selector-button">{wip_id}</ToggleButton>
+          )
+        })}
+      </ToggleButtonGroup>
+      
     </div>
+    
   );
 }
 
