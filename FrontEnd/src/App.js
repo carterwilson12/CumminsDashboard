043b0a237d3007fe1a6434b3a7688c2cc231d6ff
laguila47 <78505916+laguila47@users.noreply.github.com>
2024-02-17 14:56:32 -0500

    Initial commit
    App will connect to a mysql database. For testing using xampp to deploy mariaDB
    
    Using express.js use below command to install.
    
    npm install express mysql cors nodemon

---
+++ FrontEnd/src/App.js
@@ -0,0 +1,45 @@
+import './App.css';
+import WIPselector from './components/WIP-selector';
+import AutocompleteHint from './components/autocomplete';
+import TSNtable from './components/TSNtable';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import React, {useEffect, useState} from 'react';
+
+function App() {
+
+  const [data, setData] = useState([])
+
+  useEffect(()=>{
+    fetch('http://localhost:8081/users')
+    .then(res => res.json())
+    .then(data => setData(data))
+    .catch(err => console.log(err))
+  },[])
+
+  return (
+    <div className="App">
+      <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={2}>
+          <div className="wipselector">
+            <AutocompleteHint></AutocompleteHint>
+            {data.map((d,i) =>(
+              <WIPselector wip_id={d.azure_id}></WIPselector>
+            ))}
+            
+          </div>           
+        </Grid>
+        <Grid item xs={10}>
+          <div className="SearchBar">  
+            <TSNtable></TSNtable>
+          </div>      
+        </Grid>
+      </Grid>
+    </Box>      
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ FrontEnd/src/components/WIP-selector.js
@@ -0,0 +1,20 @@
+import { ToggleButton, ToggleButtonGroup } from '@mui/material';
+import React from 'react';
+
+
+
+
+export default function WIPselector({ wip_id }) { // Destructure wip_ids and provide a default value
+  console.log(wip_id);
+
+  // Ensure wip_ids is an array before using map
+  return (
+    
+        <ToggleButtonGroup 
+        >
+        <ToggleButton value={wip_id} className="WIP-selector-button">
+        {wip_id}
+        </ToggleButton>
+        </ToggleButtonGroup>
+  );
+}
