875faa5ee2e65846675bb63ed46ed84b9bf899e8
Collins Ramsey <collins.ramsey@gmail.com>
2024-02-22 14:46:06 -0500

    BOM table updated in App.js

--- src/App.js
+++ src/App.js
@@ -29,12 +29,23 @@ const wip_ids = ["LS12231232",
   "MS1732132124",
   "MS17323323253333333333333",]
 
+
+  /* TSN table data */
   const sample = [
     ['73213', 0.4, 1],
     ['73214', 0.1, 0],
     ['73215', 0.1, 0],
     ['73216', 0.1, 0],
     ['73217', 0.1, 0],
+  ];
+
+  /* BOM table data */
+  const sample2 = [
+    ['part 1', 'part 2', 'part 3', 'part 4', 'part 5'],
+    ['part'],
+    ['part'],
+    ['part'],
+    ['part'],
   ];
 
   
@@ -48,8 +59,8 @@ function App() {
           
         </div>
         <Grid container direction="row" justifyContent="flex-start" spacing={2}>
-          <Grid item xs={2}><WIPselector wip_ids={wip_ids}></WIPselector></Grid>
-          <Grid item xs={8}><TSNtable sample={sample}
+          <Grid item xs={2}><WIPselector wip_ids={wip_ids}/></Grid>
+          <Grid item xs={10}><TSNtable sample={sample}
         sx={{
           width: 1000,
           height: 500,
@@ -58,7 +69,8 @@ function App() {
           
         }}
       /></Grid>
-          <Grid item xs={8}><BOMtable></BOMtable></Grid>
+          <Grid item xs={2}></Grid>
+          <Grid item xs={10}><BOMtable sample2={sample2}></BOMtable></Grid>
         </Grid>
         
       
