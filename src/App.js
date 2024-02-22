8372533ac5695c1a851a0016d1ccdda82b7c99e0
carterwilson12 <wilsoncr2@g.cofc.edu>
2024-02-22 14:06:54 -0500

    WIP-selector moved into App.js

--- src/App.js
+++ src/App.js
@@ -27,18 +27,39 @@ const wip_ids = ["LS12231232",
   "MS1732213213",
   "MS1732132124",
   "MS17323323253333333333333",]
-
-  const sample = [
-    ['73213', 0.4, 0, 0, 0],
-    ['73214', 0.1, 0, 0, 0],
-    ['73215', 0.1, 0, 0, 0],
-    ['73216', 0.1, 0, 0, 0],
-    ['73217', 0.1, 0, 0, 0],
-  ];
-
+  //Function that given the currently selected WIP will return corresponding TSN Data
+  function getTSNData(currWIP){
+    if(currWIP === "LS12231232"){
+      return [
+        ['732', 0.4, 1],
+        ['7321', 0.1, 0],
+        ['73215', 0.1, 0],
+        ['73216', 0.1, 0],
+        ['88888', 0.1, 0],
+      ];
+    }else{
+      return [
+        ['73213', 0.4, 1],
+        ['73214', 0.1, 0],
+        ['73215', 0.1, 0],
+        ['73216', 0.1, 0],
+        ['73217', 0.1, 0],
+      ];
+    }
+    
+  }
   
 function App() {
+  const [currWIP, handleWIPselect] = React.useState()
+  //Handle WIP selector change
+  const handleChange = (event, newWIP) => {
+    handleWIPselect(newWIP)
+  }
+  //TSN data
+  const sample = getTSNData(currWIP);
+  
 
+  
   return (
     <div className="App">
 
@@ -47,7 +68,22 @@ function App() {
           
         </div>
         <Grid container direction="row" justifyContent="flex-start"spacing={2}>
-          <Grid item xs={2}><WIPselector wip_ids={wip_ids}></WIPselector></Grid>
+          <Grid item xs={2}><ToggleButtonGroup 
+          exclusive
+          onChange={handleChange}
+          className="WIP-list" 
+          orientation="vertical" 
+          aria-label="Vertical button group" 
+          variant="contained"
+          >
+          {Array.isArray(wip_ids) && wip_ids.map((wip_id, index) => ( // Check if wip_ids is an array
+              <ToggleButton style={{
+                  backgroundColor: currWIP === wip_id ? '#2c387e' : undefined,color: currWIP === wip_id ? 'white' : undefined
+                }}key={index} value={wip_id} className="WIP-selector-button">
+              {wip_id}
+              </ToggleButton>
+          ))}
+          </ToggleButtonGroup></Grid>
           <Grid item xs={8}><TSNtable sample={sample}
         sx={{
           width: 1000,
