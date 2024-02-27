922c04d38d3ca80e3e6eb9dfc7f28037c4e50ed7
laguila47 <78505916+laguila47@users.noreply.github.com>
2024-02-27 15:03:27 -0500

    updated code

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -14,17 +14,40 @@ const db = mysql.createConnection({
 })
 
 app.get('/', (re, res)=> {
-    return res.json("from Backend Side");
+    return res.json("response for GET request");
 })
 
-app.get('/devices', (req, res)=> {
+app.get('/wips', (req, res)=> {
     const sql = "SELECT * FROM mes_wip_info";
     db.query(sql, (err, data)=> {
-        if(err) return res.json(err);
+        if(err) {
+            return res.json(err)
+        }
         return res.json(data);
     })
 })
 
+app.get('/tsn', (req, res)=> {
+    const sql = "SELECT * FROM mes_assy_job_info";
+    db.query(sql, (err, data)=> {
+        if(err) {
+            return res.json(err)
+        }
+        return res.json(data);
+    })
+})
+
+app.get("/tsn_id/:id", (req,res)=>{
+
+    const id = req.params.id;
+     db.query("SELECT * FROM posts WHERE id = ?", id, 
+     (err,result)=>{
+        if(err) {
+        console.log(err)
+        } 
+        res.send(result)
+        });   });
+
 app.listen(8081, ()=>{
-    console.log("listening");
+    console.log("Running");
 })
